7bab6fe3aacdc7cd505e27abc25db9d8941ba139
Nathan Downs <n8downs@gmail.com>
2016-05-20 13:49:02 -0700

    Tag along on a prior Google JS load, if there is one and it's not finished

--- src/index.js
+++ src/index.js
@@ -15,24 +15,30 @@ export default (options = {}) => (TargetComponent) => (
 
     componentDidMount() {
       if (typeof window.google === 'undefined') {
-        window.googleMapsLoaded = () => {
-          scriptjs.done('google-maps-places')
+        if (typeof window.googleMapsLoaded !== 'undefined') {
+          scriptjs.ready('google-maps-places', () => {
+            this.handleLoaded(google.maps)
+          })
+        } else {
+          window.googleMapsLoaded = () => {
+            scriptjs.done('google-maps-places')
+          }
+
+          options.callback = 'googleMapsLoaded'
+
+          options.key = options.key || this.props.apiKey
+
+          const queryString = Object.keys(options).reduce((result, key) => (
+            options[key] !== null && options[key] !== undefined
+              ? (result += key + '=' + options[key] + '&')
+              : result
+          ), '?').slice(0, -1)
+
+          scriptjs('https://maps.googleapis.com/maps/api/js' + queryString)
+          scriptjs.ready('google-maps-places', () => {
+            this.handleLoaded(google.maps)
+          })
         }
-
-        options.callback = 'googleMapsLoaded'
-
-        options.key = options.key || this.props.apiKey
-
-        const queryString = Object.keys(options).reduce((result, key) => (
-          options[key] !== null && options[key] !== undefined
-            ? (result += key + '=' + options[key] + '&')
-            : result
-        ), '?').slice(0, -1)
-
-        scriptjs('https://maps.googleapis.com/maps/api/js' + queryString)
-        scriptjs.ready('google-maps-places', () => {
-          this.handleLoaded(google.maps)
-        })
       } else {
         this.handleLoaded(google.maps)
       }
